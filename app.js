aecf142d53552ea37900446a1d473239bb14a197
Solar Print Process Pvt. Ltd. <spppl.in@gmail.com>
2026-04-25 13:23:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ import { init, cleanup } from './whatsapp.js'
 import cors from 'cors'
 
 const app = express()
-
 const host = process.env.WA_SERVER_HOST || undefined
-const port = parseInt(process.env.WA_SERVER_PORT ?? 8000)
+const port = parseInt(process.env.PORT ?? process.env.WA_SERVER_PORT ?? 8000)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
